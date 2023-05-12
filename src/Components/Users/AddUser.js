5679d0a5ae1a9950e948549581134ae2347e6ad7
MrAbhishekRawat <abhishekr.rawat8@gmail.com>
2023-05-12 11:43:50 +0530

    style task6

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -2,15 +2,22 @@ import React from "react";
 
 const AddUser = (props) => {
 
-    const addUSerHandler= (event)=>{
+    const addUserHandler = (event) => {
         event.preventDefault();
     }
     return (
-        <form onSubmit={addUSerHandler}>
+        <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
             <input id="username" type="text" />
             <label htmlFor="age">Age(Years)</label>
             <input id="age" type="number" />
+            <label htmlFor="college">College Name:</label>
+
+            <select name="college" id="college">
+                <option value="col1">College 1</option>
+                <option value="col2">College 2</option>
+                <option value="col3">College 3</option>
+            </select>
 
             <button type="submit">Add User</button>
 
